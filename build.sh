40d74238ec5999dc05ffa9ed0af1e305f12fffd0
xiehuc <xiehuc@gmail.com>
2022-02-15 23:03:16 +0800

    add icns file

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 [[ ! -e /usr/local/bin/platypus ]] && echo You Should Install Platypus Command Line Tool first && exit 0
 rm -r squashfs.app
+cp /System/Library/CoreServices/DiskImageMounter.app/Contents/Resources/diskimage.icns diskcopy-doc.icns
+cp /System/Library/CoreServices/ODSAgent.app/Contents/Resources/CD.icns diskcopy.icns
 /usr/local/bin/platypus -P squashfs.platypus squashfs.app
